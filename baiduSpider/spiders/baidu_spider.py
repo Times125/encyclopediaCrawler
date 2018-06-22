0cbdecdc29f5e06d47d16cc3c7560953f51dfcad
DESKTOP-PHBFCBT\lch02 <1251680944@qq.com>
2018-06-22 17:38:58 +0800

    v 1.4 * 修复数据入库的问题；

--- baiduSpider/spiders/baidu_spider.py
+++ baiduSpider/spiders/baidu_spider.py
@@ -3,6 +3,7 @@
 @Time:   
 @Description: 百度百科爬虫
 """
+import re
 from urllib.parse import unquote
 from filters.bloom_filter import BloomFilter
 from scrapy import Request
@@ -17,8 +18,8 @@ class baiduSpider(CrawlSpider):
     base_url = "https://baike.baidu.com"
     name = 'baiduSpider'
     allowed_domains = ['baike.baidu.com']
-    start_urls = ['https://baike.baidu.com/item/操作系统']
-    # redis_key = 'mycrawler:start_urls'
+    start_urls = ['https://baike.baidu.com/item/英国跳猎犬']
+    # redis_key = 'baiduSpider:start_urls'
     rules = (
         Rule(LinkExtractor(allow=('https://baike.baidu.com/item/')), callback='parse', follow=True),
     )
@@ -36,18 +37,20 @@ class baiduSpider(CrawlSpider):
 
         title = selector.xpath("/html/head/title/text()").extract()
         if len(title) != 0:
-            items['title'] = title[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['title'] = title[0].strip().encode('utf-8', errors='ignore').decode('utf-8')
         else:
             items['title'] = 'none'
         summary = selector.xpath("//div[@class=\"lemma-summary\"]").xpath("string(.)").extract()
         if len(summary) != 0:
-            items['summary'] = summary[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmps = summary[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['summary'] = re.sub('\r\n|\n|\r', ' ', tmps)
         else:
             items['summary'] = 'none'
 
         catalog = selector.xpath("//div[@class=\"lemmaWgt-lemmaCatalog\"]").xpath("string(.)").extract()
         if len(catalog) != 0:
-            items['catalog'] = catalog[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmpc = catalog[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['catalog'] = re.sub('\r\n|\n|\r', ' ', tmpc)
         else:
             items['catalog'] = 'none'
 
@@ -58,7 +61,8 @@ class baiduSpider(CrawlSpider):
 
         description = selector.xpath("//div[@class=\"content-wrapper\"]").xpath("string(.)").extract()
         if len(description) != 0:
-            items['description'] = description[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmpd = description[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['description'] = re.sub('\r\n|\n|\r', ' ', tmpd)
         else:
             items['description'] = 'none'
 
@@ -76,26 +80,29 @@ class baiduSpider(CrawlSpider):
 
         update_time = selector.xpath("//span[@class = 'j-modified-time']").xpath("string(.)").extract()
         if len(update_time) != 0:
-            items['update_time'] = update_time[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmpu = update_time[0].strip().encode('utf-8', errors='ignore').decode('utf-8')
+            items['update_time'] = re.sub('\r\n|\n|\r', ' ', tmpu)
         else:
             items['update_time'] = 'none'
 
         reference_material = selector.xpath(
             "//dl[@class ='lemma-reference collapse nslog-area log-set-param']").xpath("string(.)").extract()
         if len(reference_material) != 0:
-            items['reference_material'] = reference_material[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmpr = reference_material[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['reference_material'] = re.sub('\r\n|\n|\r', ' ', tmpr)
         else:
             items['reference_material'] = 'none'
 
         item_tag = selector.xpath("//dd[@id = \"open-tag-item\"]").xpath("string(.)").extract()
         if len(item_tag) != 0:
-            items['item_tag'] = item_tag[0].encode('utf-8', errors='ignore').decode('utf-8')
+            tmpi = item_tag[0].encode('utf-8', errors='ignore').decode('utf-8')
+            items['item_tag'] = re.sub('\r\n|\n|\r', ' ', tmpi)
         else:
             items['item_tag'] = 'none'
-        # print(items['keywords_url'])
         # 布隆过滤器过滤已经下载的词条
         # urls = [u for u in items['keywords_url'] if BloomFilter().is_contain(u) is False]
         old_urls = items['keywords_url']
+        print(old_urls)
         yield items
         for i in old_urls:
             new_url = self.base_url + i
